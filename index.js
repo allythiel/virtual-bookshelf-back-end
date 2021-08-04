d138d81bb62786bebab2779e873bdafaad180d6d
allythiel <allythiel@gmail.com>
2021-08-04 17:04:15 -0500

    Update Index.js to reference User Routes

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./startup/db');
 const app = express();
+const users = require('./routes/users');
+
 
 connectDB();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/users', users);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
---
+++ routes/users.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const {User, Comment, validateUser, validateComment } = require('../models/user');
+
+
+
+
+
+
+module.exports = router;
