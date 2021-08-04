d4927b62802c26b171db932f4dbdb5c843b988be
allythiel <allythiel@gmail.com>
2021-08-04 17:08:16 -0500

    Add Get All Comments Route

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,15 @@ const express = require('express');
 const router = express.Router();
 const {User, Comment, validateUser, validateComment } = require('../models/user');
 
-
-
-
+////////////////////////GET ALL COMMENTS ////////////////////////
+router.get('/', async (req, res) => {
+    try {
+       const comments = await Comment.find();
+       return res.send(comments);
+    } catch (ex) {
+       return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+ });
 
 
 module.exports = router;
