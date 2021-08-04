7f959268c69c2b59a12ec664cac7b5db1b7c6c6f
allythiel <allythiel@gmail.com>
2021-08-04 17:14:07 -0500

    Add Get by Book ID

--- routes/users.js
+++ routes/users.js
@@ -12,5 +12,20 @@ router.get('/', async (req, res) => {
     }
  });
 
+ ////////////////////////GET COMMENTS BY BOOK ID ////////////////////////
+ router.get('/:bookId', async (req, res) => {
+    try {
+       const comment = await Comment.find({ bookId: req.params.bookId });
+       console.log(comment);
+       if (!comment)
+          return res.status(400).send(`The comment with id "${req.params.bookId}" d
+    oes not exist.`);
+       return res.send(comment);
+    } catch (ex) {
+       return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+ });
+
+
 
 module.exports = router;
