ba683b23be35f0775b89205f4b7c498e37a5ad7b
allythiel <allythiel@gmail.com>
2021-08-16 21:31:12 -0500

    Add Bookshelf Schema

--- models/user.js
+++ models/user.js
@@ -13,13 +13,33 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true, minlength: 5, maxLength: 100},
     password: { type: String, required: true, minLength: 5, maxLength: 20},
     email: { type: String, required: true, minLength: 5, maxLength: 50},
-    comments: {commentSchema}
+    comments: {commentSchema},
+    bookshelf: {bookshelfSchema}
 });
+
+const bookshelfSchema = new mongoose.Schema({
+    kind: { type: String },
+    book_id: { type: String },
+    etag: { type: String },
+    selfLink: { type: String },
+    volumeInfo: {
+        title: { type: String},
+        authors: [{ type: String}],
+        description: { type: String, minLength: 5, maxLength: 500},
+        averageRating: {type: Number},
+        ratingsCount: { type: Number },
+        imageLinks: {
+            smallThumbnail: { type: String},
+            thumbnail: {type: String},
+        }
+    }
+})
+
 
 ////////////////////////////////////////////////////////////////////////////
 const Comment = mongoose.model("Comment", commentSchema);
 const User = mongoose.model("User", userSchema);
-
+const Bookshelf = mongoose.model("Bookshelf", bookshelfSchema);
 
 /////////////////////////VALIDATION//////////////////////////////////////////
 
@@ -41,11 +61,31 @@ function validateComment(comment){
     return schema.validate(comment);
 }
 
+function validateBookshelf(bookshelf){
+    const schema = Joi.object({
+        kind: Joi.string(),
+        book_id: Joi.string(),
+        etag: Joi.string(),
+        selfLink: Joi.string(),
+        volumeInfo: {
+            title: Joi.string(),
+            authors: [Joi.string()],
+            description: Joi.string().min(5).max(500),
+            averageRating: Joi.number(),
+            ratingsCount: Joi.number(),
+            imageLinks: {
+                smallThumbnail: Joi.string(),
+                thumbnail: Joi.string(),
+    })
+    return schema.validate(bookshelf);
+}
+
 ////////////////////////////////////////////////////////////////////////////
 
 module.exports = {
     User: User,
     Comment: Comment,
+    Bookshelf: Bookshelf,
     validateUser: validateUser,
     validateComment: validateComment
 }
